50b737dcda10eb08f9cf81edc2a056984dd7e288
Davy <davy@datalink.be>
2025-11-14 09:31:53 +0100

    refactor: install typescript, eslint and prettier

---
+++ resources/ts/types/global.d.ts
@@ -0,0 +1,20 @@
+// Extend ImportMeta interface for Vite...
+declare module 'vite/client' {
+    interface ImportMetaEnv {
+        readonly VITE_APP_NAME: string;
+        [key: string]: string | boolean | undefined;
+    }
+
+    interface ImportMeta {
+        readonly env: ImportMetaEnv;
+        readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
+    }
+}
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $inertia: typeof Router;
+        $page: Page;
+        $headManager: ReturnType<typeof createHeadManager>;
+    }
+}
